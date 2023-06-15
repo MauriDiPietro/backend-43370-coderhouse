c5b51d6d407a1660bea93029ce2d963d58526ea5
MauriDiPietro <dipietro.jm@gmail.com>
2023-06-14 22:26:38 -0300

    cambios

--- 08 - EXPRESS AVANZADO/src/server.js
+++ 08 - EXPRESS AVANZADO/src/server.js
@@ -33,7 +33,69 @@ app.get('/users/:idUser', async(req, res)=>{
     }
 });
 
-app.post
+app.get('/search', async(req, res)=>{
+    try {
+        const { idUser } = req.query;
+        const user = await userManager.getUserById(Number(idUser));
+        if(user){
+            res.json(user)
+        } else {
+            res.status(400).json({message: 'User not found'});
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.post('/users', async(req, res)=>{
+    try {
+        // console.log(req.body);
+        // const user = req.body
+        const { firstname, lastname, email } = req.body;
+        const user = {
+            firstname,
+            lastname, 
+            email,
+        }
+        const newUser = await userManager.createUser(user);
+        res.json(newUser);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.put("/users/:idUser", async (req, res) => {
+  try {
+    const user = req.body;
+    const { idUser } = req.params;
+    const idNumber = parseInt(idUser);
+    const userExist = await userManager.getUserById(idNumber);
+    if (userExist) {
+      await userManager.updateUser(user, idNumber);
+      res.json({ message: `User id: ${idNumber} updated` });
+    } else {
+      res.status(400).json({ message: `User id: ${idNumber} Not found` });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete('/users/:idUser', async(req, res)=>{
+    try {
+        const { idUser } = req.params;
+        const idNumber = parseInt(idUser);
+        const userExist = await userManager.getUserById(idNumber);
+        if(userExist){
+            await userManager.deleteUser(idNumber);
+            res.status(200).json({message: `User ${idNumber} deleted ok`});
+        } else {
+            res.json({message: `User ${idNumber} not found`})
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 app.listen(8080, ()=>{
     console.log('Server ok on port 8080');
