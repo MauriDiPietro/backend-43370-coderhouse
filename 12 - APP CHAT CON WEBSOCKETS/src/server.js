8cb83dfef911df6bb9ee6947c9151c32c2140049
MauriDiPietro <dipietro.jm@gmail.com>
2023-07-03 20:44:37 -0300

    clase 12 - inprogress

---
+++ 12 - APP CHAT CON WEBSOCKETS/src/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { __dirname } from './utils.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import handlebars from 'express-handlebars';
+import { Server } from 'socket.io';
+import viewsRouter from './routes/views.router.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler);
+
+app.engine('handlebars', handlebars.engine()); 
+app.set('view engine', 'handlebars');  
+app.set('views', __dirname+'/views');  
+
+app.use('/chat', viewsRouter);
+
+const httpServer = app.listen(8080, ()=>{
+    console.log('🚀 Server listening on port 8080');
+});
+
+const socketServer = new Server(httpServer);
