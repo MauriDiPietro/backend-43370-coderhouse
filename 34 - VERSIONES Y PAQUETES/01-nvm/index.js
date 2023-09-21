22eeba31f4289a04ee50004e2d0822607dee7ad2
MauriDiPietro <dipietro.jm@gmail.com>
2023-09-20 22:28:45 -0300

    clase 34

---
+++ 34 - VERSIONES Y PAQUETES/01-nvm/index.js
@@ -0,0 +1,6 @@
+console.log('version Node: ', process.version);
+console.log('hola estoy ejecutandome con --watch');
+
+// .nvmrc
+// windows --> nvm use $(Get-Content .nvmrc)
+// linux --> nvm use
---
+++ 34 - VERSIONES Y PAQUETES/06-prueba-libreria/index.js
@@ -0,0 +1,3 @@
+const calculadora = require('super-calc123456');
+
+console.log(calculadora.sumar(1, 2))
